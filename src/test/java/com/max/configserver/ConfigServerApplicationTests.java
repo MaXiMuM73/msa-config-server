57c31f89f370c8cffedcb90926e4bcc2e5e357a1
Maksim Sunoplya <maksim.sunoplya@nordclan.com>
2022-07-04 23:41:59 +0400

    Init commit

---
+++ src/test/java/com/max/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
